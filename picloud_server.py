8c960e85f7271dfc7f84b397dd5542c78f37989e
Michael Verhulst <michael@terminallabs.com>
2013-04-20 23:12:04 -0500

    some polish

--- picloud_server.py
+++ picloud_server.py
@@ -26,7 +26,8 @@ class PycloudMiddleman(tornado.web.RequestHandler):
         sub = str(int(sub['substring']))
 
         number_of_nodes = 10
-        charicters_processed = 10000000 * number_of_nodes
+        charicters_in_node = 10000000 
+        charicters_processed = charicters_in_node * number_of_nodes
         nodes = map(lambda x: "%03d" % x,list(xrange(number_of_nodes)))
         args = map(lambda x: [sub,x],nodes)
 
@@ -37,7 +38,9 @@ class PycloudMiddleman(tornado.web.RequestHandler):
 
         total_computation_time = sum(map(lambda x: x[0],from_cloud))
         graph_data = map(lambda x: [int(x[2]),x[1]],from_cloud)
-        first_occurance = filter(lambda x: x != -1,map(lambda x: x[3],from_cloud))[0]
+        first_occurances_in_nodes = map(lambda x: x[3],from_cloud)
+        first_occurance_node = first_occurances_in_nodes.index(next( i for i in first_occurances_in_nodes if i != -1))
+        first_occurance = filter(lambda x: x != -1,first_occurances_in_nodes)[0] + (charicters_in_node * (first_occurance_node + 1))
         average_node_time = total_computation_time / number_of_nodes
         total_instances = sum(map(lambda x: x[1],from_cloud))
         frequency = total_instances / float(charicters_processed)
@@ -49,16 +52,16 @@ class PycloudMiddleman(tornado.web.RequestHandler):
 
         data_dict = {}
         data_dict['graph_data'] = graph_data
-        data_dict['total_computation_time'] = total_computation_time
-        data_dict['charicters_processed'] = charicters_processed
+        data_dict['total_computation_time'] = '%.4e' % total_computation_time
+        data_dict['charicters_processed'] = '%e4' % charicters_processed
         data_dict['first_occurance'] = first_occurance
-        data_dict['average_node_time'] = average_node_time
+        data_dict['average_node_time'] = '%.4e' % average_node_time
         data_dict['total_instances'] = total_instances
         data_dict['frequency'] = frequency
-        data_dict['period'] = period
+        data_dict['period'] = int(period)
         data_dict['average_instances'] = average_instances
-        data_dict['average_time_per_node'] = average_time_per_node
-        data_dict['total_time_for_entire_process'] = total_time_for_entire_process
+        data_dict['average_time_per_node'] = '%.4e' % average_time_per_node
+        data_dict['total_time_for_entire_process'] = '%.4e' % total_time_for_entire_process
         
         return s.write(base64.b64encode(json.dumps(data_dict)))
 
