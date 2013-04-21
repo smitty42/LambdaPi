acfc4fda7d9a129210e67b7fc3a74766b2140894
Michael Verhulst <michael@terminallabs.com>
2013-04-20 22:16:50 -0500

    Woo, actually taking in ready data and doing stuff!

--- picloud_server.py
+++ picloud_server.py
@@ -22,27 +22,58 @@ class PycloudMiddleman(tornado.web.RequestHandler):
     def get(s):
         sub = s.get_argument('substring')
         sub = base64.b64decode(str(sub))
-        
-        jids = cloud.call(read_picloud_file)
-        
+        sub = json.loads(sub)
+        sub = str(int(sub['substring']))
+
+        number_of_nodes = 10
+        charicters_processed = 10000000 * number_of_nodes
+        nodes = map(lambda x: "%03d" % x,list(xrange(number_of_nodes)))
+        args = map(lambda x: [sub,x],nodes)
+
+        start = time.time()
+        jids = cloud.map(pi_string_search,args)        
         from_cloud = cloud.result(jids)
+        end = time.time()
+
+        total_computation_time = sum(map(lambda x: x[0],from_cloud))
+        graph_data = map(lambda x: [int(x[2]),x[1]],from_cloud)
+        first_occurance = filter(lambda x: x != -1,map(lambda x: x[3],from_cloud))[0]
+        average_node_time = total_computation_time / number_of_nodes
+        total_instances = sum(map(lambda x: x[1],from_cloud))
+        frequency = total_instances / float(charicters_processed)
+        period = 1 / frequency
+        average_instances = total_instances / float(number_of_nodes)
+        average_time_per_node = total_computation_time / float(number_of_nodes)
+        total_time_for_entire_process = end - start
         
-        graph_data = [[0, 3], [1, 8], [3, 5], [4, 13],
-                [5, 3], [6, 8], [7, 5], [8, 13],
-				[9, 3], [10, 8], [12, 5], [13, 13]];
-                
-        data_dict = {"graph_data":graph_data}
-        data_dict['total_time'] = from_cloud
-        data_dict['data_processed'] = 987654321
+
+        data_dict = {}
+        data_dict['graph_data'] = graph_data
+        data_dict['total_computation_time'] = total_computation_time
+        data_dict['charicters_processed'] = charicters_processed
+        data_dict['first_occurance'] = first_occurance
+        data_dict['average_node_time'] = average_node_time
+        data_dict['total_instances'] = total_instances
+        data_dict['frequency'] = frequency
+        data_dict['period'] = period
+        data_dict['average_instances'] = average_instances
+        data_dict['average_time_per_node'] = average_time_per_node
+        data_dict['total_time_for_entire_process'] = total_time_for_entire_process
         
         return s.write(base64.b64encode(json.dumps(data_dict)))
 
-def read_picloud_file():
-    start = time.time()
-    picloud_file = cloud.bucket.getf('pi_parts000', prefix='classB')
+def pi_string_search(data):
+    sub = data[0]
+    node = data[1]
+    picloud_file = cloud.bucket.getf('pi_parts%s'% (node), prefix='classB')
     pi = picloud_file.read()
+
+    start = time.time()
+    instances = pi.count(sub)
+    first_occurance = pi.find(sub)
     end = time.time()
-    return end - start
+    delta = end - start
+    return [delta, instances, node, first_occurance]
 
 def main():
     tornado.options.parse_command_line()
