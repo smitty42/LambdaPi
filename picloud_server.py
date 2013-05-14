913a85b0ace9f03ea25d8e7937ec7d669ac0f5d0
Chris Smith <smith@terminallabs.com>
2013-05-13 19:19:51 -0500

    touch up

--- picloud_server.py
+++ picloud_server.py
@@ -28,7 +28,7 @@ class PycloudMiddleman(tornado.web.RequestHandler):
         number_of_nodes = 10
         characters_in_node = 10000000 
         characters_processed = characters_in_node * number_of_nodes
-        nodes = map(lambda x: "%03d" % x,list(xrange(number_of_nodes)))
+        nodes = map(lambda x: "%03d" % x,xrange(number_of_nodes))
         args = map(lambda x: [sub,x],nodes)
 
         start = time.time()
@@ -39,9 +39,10 @@ class PycloudMiddleman(tornado.web.RequestHandler):
         total_computation_time = sum(map(lambda x: x[0],from_cloud))
         graph_data = map(lambda x: [int(x[2]),x[1]],from_cloud)
         first_occurances_in_nodes = map(lambda x: x[3],from_cloud)
-        if filter(lambda x: x != -1, first_occurances_in_nodes) != []:
-            first_occurance_node = first_occurances_in_nodes.index(next( i for i in first_occurances_in_nodes if i != -1))
-            first_occurance = filter(lambda x: x != -1,first_occurances_in_nodes)[0] + (characters_in_node * (first_occurance_node + 1))
+        first_occurrences = filter(lambda x: x != -1, first_occurrences_in_nodes)
+        if first_occurrences:
+            first_occurrence_node = first_occurrences_in_nodes.index(next( i for i in first_occurrences_in_nodes if i != -1))
+            first_occurrence = first_occurrences[0] + (characters_in_node * (first_occurrence_node + 1))
             average_node_time = total_computation_time / number_of_nodes
             total_instances = sum(map(lambda x: x[1],from_cloud))
             frequency = total_instances / float(characters_processed)
@@ -50,8 +51,8 @@ class PycloudMiddleman(tornado.web.RequestHandler):
             average_time_per_node = total_computation_time / float(number_of_nodes)
             total_time_for_entire_process = end - start
         else:
-            first_occurance_node = 'N/A'
-            first_occurance = 'N/A'
+            first_occurrence_node = 'N/A'
+            first_occurrence = 'N/A'
             average_node_time = total_computation_time / number_of_nodes
             total_instances = 0
             frequency = 0
@@ -65,7 +66,7 @@ class PycloudMiddleman(tornado.web.RequestHandler):
         data_dict['graph_data'] = graph_data
         data_dict['total_computation_time'] = '%.4e' % total_computation_time
         data_dict['characters_processed'] = '%e' % characters_processed
-        data_dict['first_occurance'] = first_occurance
+        data_dict['first_occurrence'] = first_occurrence
         data_dict['average_node_time'] = '%.4e' % average_node_time
         data_dict['total_instances'] = total_instances
         data_dict['frequency'] = frequency
@@ -84,10 +85,11 @@ def pi_string_search(data):
 
     start = time.time()
     instances = pi.count(sub)
-    first_occurance = pi.find(sub)
+    first_occurrence = pi.find(sub)
     end = time.time()
     delta = end - start
-    return [delta, instances, node, first_occurance]
+    return [delta, instances, node, first_occurrence]
+
 
 def main():
     tornado.options.parse_command_line()
